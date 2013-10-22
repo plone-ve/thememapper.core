79e98643a6e95b1481190958b16cfe7811c4835e
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2013-10-22 18:33:35 -0430

    Added i18n support and Spanish l10n

--- thememapper/core/__init__.py
+++ thememapper/core/__init__.py
@@ -1,1 +1,2 @@
-# This package may contain traces of nuts
+from flask.ext.babel import Babel
+babel = Babel(thememapper.core)
--- thememapper/core/model/navigation.py
+++ thememapper/core/model/navigation.py
@@ -1,12 +1,13 @@
+from flask.ext.babel import gettext
 import copy
 
 class _NavigationModel:
 
     def __init__(self):
         self.nav_items = [
-            {'slug':'home','text':      'Home',                 'url':'/',              'class':'',     'target':'_self'},
-            {'slug':'editor',     'text': 'Editor', 'url':'/editor/',        'class':'' ,'target':'_self'},
-            {'slug':'settings',    'text': 'Settings','url':'/settings/', 'class':'',     'target':'_self'},
+            {'slug':'home','text':      gettext('Home'),                 'url':'/',              'class':'',     'target':'_self'},
+            {'slug':'editor',     'text': gettext('Editor'), 'url':'/editor/',        'class':'' ,'target':'_self'},
+            {'slug':'settings',    'text': gettext('Settings'),'url':'/settings/', 'class':'',     'target':'_self'},
         ]
 
     def get_items(self,active='home',extra_items=None):
